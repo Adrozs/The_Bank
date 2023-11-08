fb59d8431efe537900dc82fcfdce02da0c3a1ab7
Adrozs <adrozsahegyi@gmail.com>
2023-11-08 21:23:35 +0100

    Added clarifying comments
    Commented and clarified what each section of code does to increase readability

--- Utilities/DbHelpers.cs
+++ Utilities/DbHelpers.cs
@@ -10,19 +10,23 @@ namespace The_Bank.Utilities
 {
     internal class DbHelpers
     {
+        // Creates a list of all users in the database
         public static List<User> GetAllUsers(BankContext context)
         {
             List<User> users = context.Users.ToList();
             return users;   
         }
 
+        // Adds and saves user to database
         public static bool AddUser(BankContext context, User user)
         {
+            // Tries to save changes to database
             context.Users.Add(user);
             try
             {
                 context.SaveChanges();
             }
+            // If wasn't possible to save, print error and return
             catch (Exception ex) 
             {
                 Console.WriteLine($"Error adding user: {ex}");
@@ -31,13 +35,16 @@ namespace The_Bank.Utilities
             return true;
         }
 
+        // Adds and saves account to database
         public static bool AddAccount(BankContext context, Account account)
         {
+            // Tries to save changes to database
             context.Accounts.Add(account);
             try
             {
                 context.SaveChanges();
             }
+            // If wasn't possible to save, print error and return
             catch (Exception ex)
             {
                 Console.WriteLine($"Error adding account: {ex}");
