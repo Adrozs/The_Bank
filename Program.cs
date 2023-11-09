1b4482923705302bf548e586e7668717a5e2852e
Adrozs <adrozsahegyi@gmail.com>
2023-11-09 19:18:28 +0100

    Added checks and mechanics to freeze and unfreeze users.
    -Added variable to save the unfreeze time to
    
    -Added some clarifying comments to separate login, admin and user parts of the code
    
    -Before any login attempt is made we check if the user is frozen or not.
    If it is frozen it prints out error message and checks if user can be unfrozen and unfreezes them if yes.
    NOTE! This could be optimized in the future to perhaps do the "can be unfrozen check" before so user don't get an error even when their "time out" time has passed as we do the check in the bottom of the code.
    
    -Changed in the loop so if user enters pin wrong 3 times, account is frozen

--- Program.cs
+++ Program.cs
@@ -7,22 +7,27 @@ namespace The_Bank
         static void Main(string[] args)
         {
             // Welcome phrase
-            Console.WriteLine("Welcome to bank! \n");
+            Console.WriteLine("Welcome to the bank! \n");
 
-            // Counter to keep track of login attempts - initalize at value and decrement on each wrong attempt
+            // Initalize counter to keep track of login attempts
             int loginAttempts = 2;
+
+            // Declare date time variable to be used to keep track of when a user can be unfrozen if they've frozen their account
+            DateTime UnFreezeTime;
+
 
             // Loop until user chooses to exit program
             // TODO: Add command to exit program
             while (true)
             {
-                // Login screen
+                // LOGIN SCREEN
                 Console.WriteLine("Login ");
                 Console.Write("Name: ");
                 string userName = Console.ReadLine();
                 Console.Write("PIN: ");
                 string pin = Console.ReadLine();
 
+                // ADMIN LOGIN
                 // Checks if username is admin and if pin is correct - if yes it calls the admin menu
                 // TODO?: change so certain users are admin instead of a pre-determined admin? Discuss in group.
                 if (userName == "admin")
@@ -35,41 +40,42 @@ namespace The_Bank
                         AdminFunctions.DoAdminTasks();
                 }
 
-                // Check if user login is correct 
-                if (DbHelpers.VerifyLogin(userName, pin))
+                // USER LOGIN
+                // Check if account is NOT freezed
+                if (!AccountFreezed.IsFreezed(userName))
                 {
-                    UserFunctions.UserMenu(userName);
+                    // Check if user login is correct 
+                    if (DbHelpers.VerifyLogin(userName, pin))
+                    {
+                        UserFunctions.UserMenu(userName);
+                    }
+                    else if (loginAttempts > 0)
+                    {
+                        Console.WriteLine($"Username or pin is invalid. {loginAttempts} tries left.");
+                        loginAttempts--;
+                    }
+                    // If too many invalid login attempts were made freeze account for x amount of time
+                    else
+                    {
+                        Console.WriteLine("Too many invalid attemtps. Please try again in a few minutes.");
+
+                        // Freeze account and set time that account is frozen for
+                        UnFreezeTime = DateTime.Now.AddMinutes(3);
+                        AccountFreezed.FreezeUser(userName, UnFreezeTime);
+                    }
                 }
-                else if (loginAttempts > 0)
-                {
-                    Console.WriteLine($"Username or pin is invalid. {loginAttempts} tries left.");
-                    loginAttempts--;
-                }
-                // If too many invalid login attempts were made exit program
+                // If account IS freezed
                 else
                 {
-                    Console.WriteLine("Too many invalid attempts.");
-                    Thread.Sleep(500);
-                    Console.WriteLine("Shutting down in...");
-                    Thread.Sleep(1000);
-                    Console.WriteLine("3");
-                    Thread.Sleep(1000);
-                    Console.WriteLine("2");
-                    Thread.Sleep(1000);
-                    Console.WriteLine("1");
-                    Thread.Sleep(1000);
-                    return;
+                    Console.WriteLine("Too many invalid attemtps. Please try again in a few minutes.");
+
+                    // Checks if account can be unfrozen and unfreezes it if yes, does nothing if no
+                    AccountFreezed.UnFreezeUser(userName);
                 }
 
                 // Newline for text formatting
                 Console.WriteLine();
             }
-                
-
-
-
-
-
         }
     }
 }
