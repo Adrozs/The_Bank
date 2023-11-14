fae6cad2e5a450efca96d4be38f8e92f91919572
Fady <fadynhatta@gmail.com>
2023-11-14 11:51:33 +0100

    Mapped the StockPrice to account and user. And added functionality for the actual investment.
    * CurrentPrice is Dynamic and OpeningPrice is the start value. PurchasePrice is a middleground. What the User actually buys for.

---
+++ Models/StockPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Bank.Models
+{
+    public class StockPrice
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string StockName { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal OpeningPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal PurchasePrice { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
