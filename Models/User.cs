1640c2d98ec22b1d92d11a12112818778b4cae6a
Fady <fadynhatta@gmail.com>
2023-11-14 11:47:13 +0100

    Added the CryptoInvestments and StockPrices hook to their respective tables.

--- Models/User.cs
+++ Models/User.cs
@@ -13,8 +13,10 @@ namespace The_Bank.Models
         public string Pin { get; set; }
         public bool IsFreezed { get; set; }
         public DateTime UnfreezeTime { get; set; }
+        public List<CryptoInvestment> CryptoInvestments { get; set; }
 
 
         public virtual ICollection<Account> Accounts { get; set; }
+        public virtual ICollection<StockPrice> StockPrices { get; set; }
     }
 }
