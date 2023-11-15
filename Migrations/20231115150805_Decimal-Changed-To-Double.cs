ed5e3584c601b8887ad6c8a18278d057d11956bc
adrozs <adrozsahegyi@gmail.com>
2023-11-15 16:08:28 +0100

    Changed balance from decimal to double in the db to be coherent with the rest of the code

---
+++ Migrations/20231115150805_Decimal-Changed-To-Double.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace The_Bank.Migrations
+{
+    public partial class DecimalChangedToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Balance",
+                table: "Accounts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
