ab37ad0420aa264b91ff04b87423b9c3770fe7af
Adrozs <adrozsahegyi@gmail.com>
2023-11-16 19:20:35 +0100

    Changed the name of IsCustomer to VerifyUserLogin match the VerifyAdminLogin method in naming convention to make code more readable.

--- UserFunctions.cs
+++ UserFunctions.cs
@@ -133,7 +133,7 @@ namespace The_Bank
             string customerPin = MenuFunctions.CursorReadLine();
             Console.ResetColor();
             Console.WriteLine();
-            if (DbHelpers.IsCustomer(context, userName, customerPin))
+            if (DbHelpers.VerifyUserLogin(context, userName, customerPin))
             {
                 ViewAccountInfo(context, userName);
                 Console.Write("\t\tChoose account to withdraw: ");
@@ -514,7 +514,7 @@ namespace The_Bank
                 }
 
                 // If pin matches login info, break out of loop
-                if (DbHelpers.IsCustomer(context, username, currentPin))
+                if (DbHelpers.VerifyUserLogin(context, username, currentPin))
                     break;
                 else
                     Console.WriteLine("Error! Wrong PIN. Try again. \n");
