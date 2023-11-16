0ed8e88fffbc47f11a33730181f2b5aa78b5030a
Fady <fadynhatta@gmail.com>
2023-11-16 06:11:16 +0100

    Added a new Migration in lieu of the update to the Account class.

--- Migrations/20231116051008_Currency.Designer.cs
+++ Migrations/20231116051008_Currency.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using The_Bank.Data;
 
@@ -10,9 +11,10 @@ using The_Bank.Data;
 namespace The_Bank.Migrations
 {
     [DbContext(typeof(BankContext))]
-    partial class BankContextModelSnapshot : ModelSnapshot
+    [Migration("20231116051008_Currency")]
+    partial class Currency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
